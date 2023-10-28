89e392eb5160106bd1aba8f020ae113cc2a3d2d9
whatToUseThisFor <130185104+whatToUseThisFor@users.noreply.github.com>
2023-10-28 13:17:46 -0500

    Update wordle bot.py
    added full support for yellow boxes, added algorithm to rate words based on character frequency.

--- wordle bot.py
+++ wordle bot.py
@@ -1,4 +1,3 @@
-#open file so that I don't have to write 2315 words into a list manually
 file = open("possibleWordleWords.txt", "r")
 contents = file.readlines()
 for i in range(len(contents)):
@@ -10,9 +9,15 @@ print(contents)
 requiredLetters = input("What are the required letters? (don't separate them using anything) ")
 unavailableLetters = input("What are the unavailable letters? (don't separate them using anything) ")
 order = input('What is the order of the word? (ex. if "a" is the first letter an you do not know the other letters, you would type a----) ')
+order2 = input("Where do letters NOT go (same as last one, except put five rows, and you put letters that turn yellow instead of green) ")
+order3 = input("")
+order4 = input("")
+order5 = input("")
+order6 = input("")
 
-#turn variables into lists to make it easier to handle
+#turn variables into lists to make it easier
 orderList = []
+orderList2 = []
 requiredLetterList = []
 unavailableLetterList = []
 
@@ -27,6 +32,21 @@ if len(unavailableLetters) > 0:
 
 for i in range(5):
     orderList.append(order[i])
+    
+for i in range(5):
+    orderList2.append(order2[i])
+
+for i in range(5):
+    orderList2.append(order3[i])
+    
+for i in range(5):
+    orderList2.append(order4[i])
+    
+for i in range(5):
+    orderList2.append(order5[i])
+    
+for i in range(5):
+    orderList2.append(order6[i])
     
 #start narrowing down words
 needToDelete = []
@@ -65,6 +85,124 @@ for i in contents:
 for i in needToDelete:
     contents.remove(i)
     
-#print the possible words
-print(len(contents))
-print(contents)
+needToDelete = []
+    
+for i in contents:
+    if i[0] == str(orderList2[0]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[1] == str(orderList2[1]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[2] == str(orderList2[2]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[3] == str(orderList2[3]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[4] == str(orderList2[4]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[0] == str(orderList2[5]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[1] == str(orderList2[6]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[2] == str(orderList2[7]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[3] == str(orderList2[8]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[4] == str(orderList2[9]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[0] == str(orderList2[10]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[1] == str(orderList2[11]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[2] == str(orderList2[12]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[3] == str(orderList2[13]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[4] == str(orderList2[14]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[0] == str(orderList2[15]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[1] == str(orderList2[16]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[2] == str(orderList2[17]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[3] == str(orderList2[18]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[4] == str(orderList2[19]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[0] == str(orderList2[20]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[1] == str(orderList2[21]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[2] == str(orderList2[22]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[3] == str(orderList2[23]) and i not in needToDelete:
+        needToDelete.append(i)
+    if i[4] == str(orderList2[24]) and i not in needToDelete:
+        needToDelete.append(i)
+            
+for i in needToDelete:
+    contents.remove(i)
+#order list into favorites first
+favorites = []
+scores = {} 
+for word in contents:
+    score = 0
+    if "a" in word:
+        score += 979
+    if "b" in word:
+        score += 281
+    if "c" in word:
+        score += 477
+    if "d" in word:
+        score += 393
+    if "e" in word:
+        score += 1233
+    if "f" in word:
+        score += 230
+    if "g" in word:
+        score += 311
+    if "h" in word:
+        score += 389
+    if "i" in word:
+        score += 671
+    if "j" in word:
+        score += 27
+    if "k" in word:
+        score += 210
+    if "l" in word:
+        score += 719
+    if "m" in word:
+        score += 316
+    if "n" in word:
+        score += 575
+    if "o" in word:
+        score += 754
+    if "p" in word:
+        score += 367
+    if "q" in word:
+        score += 29
+    if "r" in word:
+        score += 899
+    if "s" in word:
+        score += 669
+    if "t" in word:
+        score += 729
+    if "u" in word:
+        score += 467
+    if "v" in word:
+        score += 153
+    if "w" in word:
+        score += 195
+    if "x" in word:
+        score += 37
+    if "y" in word:
+        score += 425
+    if "z" in word:
+        score += 40
+    scores[word] = score
+    
+scores = dict(sorted(scores.items(), key = lambda item: item[1], reverse = True))
+
+favorites = list(scores.keys())
+
+print(len(favorites))
+print(favorites)
